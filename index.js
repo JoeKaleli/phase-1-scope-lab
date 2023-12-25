e471acb662055df92fdc7a69430166ea9e38eb9f
JoeKaleli <kalelimburu9@gmail.com>
2023-12-25 05:53:12 +0300

    Done with scope lab

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution in this file!
+// Declaring Customer Name
+var customerName = 'john';
+
+// Uppercase Customer Name Function
+function upperCaseCustomerName() {
+    let uppercase = customerName.toUpperCase();
+    return uppercase;
+}
+console.log(upperCaseCustomerName());
+
+// Set Best Customer Name Function
+function setBestCustomer() {
+    var bestCustomer = 'not bob';
+    return bestCustomer;
+}
+console.log(setBestCustomer());
+
+
+// OverWrite Best Customer Function
+function overwriteBestCustomer() {
+    let bestCustomer = 'joe';
+    return bestCustomer;
+}
+console.log(overwriteBestCustomer());
+
+
+
+// Declaring in global scope
+const leastFavouriteCustomer = 'kaleli'
+
+
+// Function Least Favourite Customer
+function changeLeastFavouriteCustomer() {
+    let leastFavouriteCustomer = 'Lebron';
+    return leastFavouriteCustomer;
+}
+
+console.log(changeLeastFavouriteCustomer());
